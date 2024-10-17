acb0922075a597faa9b3d28496818d921eb46129
mr-moosebear <mr.moosebear86@gmail.com>
2024-10-17 02:46:28 -0400

    Add files via upload

---
+++ scream_jam/Characters/player_character.gd
@@ -0,0 +1,30 @@
+extends CharacterBody2D
+
+@export var speed = 200.0
+
+func get_input():
+	var direction = Input.get_vector("left", "right", "up", "down")
+	if direction == Vector2(1, 0):
+		velocity = direction * speed
+		$AnimatedSprite2D.flip_h = false
+		$AnimatedSprite2D.play("walk")
+		
+	elif direction == Vector2(-1, 0):
+		velocity = direction * speed
+		$AnimatedSprite2D.flip_h = true
+		$AnimatedSprite2D.play("walk")
+
+	elif direction == Vector2(0, -1):
+		velocity = direction * speed
+		$AnimatedSprite2D.play("walk_up")
+	
+	elif direction == Vector2(0, 1):
+		velocity = direction * speed
+		$AnimatedSprite2D.play("walk_down")
+	else:
+		velocity = Vector2(0, 0)
+		$AnimatedSprite2D.play("idle")
+
+func _physics_process(delta: float) -> void:
+	get_input()
+	move_and_slide()
