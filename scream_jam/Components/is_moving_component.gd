9ddfbac2b095554ea8f98aa941551c7090c00b54
Mr-Moosebear <mr.moosebear86@gmail.com>
2024-10-23 03:27:42 -0400

    Very Close

--- scream_jam/Components/is_moving_component.gd
+++
@@ -1,9 +0,0 @@
-class_name CompIsMoving
-extends Node
-
-@export var body : CharacterBody2D
-var is_moving : bool = true
-
-func _physics_process(delta: float) -> void:
-	if body.velocity == Vector2(0, 0):
-		is_moving = false
--- scream_jam/Enemies/gremlin_two.gd
+++ scream_jam/Enemies/gremlin_two.gd
@@ -1,5 +1,6 @@
 extends CharacterBody2D
-# TODO: Make a decision when both left/right exit on choosing left or right
+
+var decision = null
 
 func _physics_process(delta: float) -> void:
 	if velocity == Vector2(0, 0):
@@ -9,16 +10,12 @@ func _physics_process(delta: float) -> void:
 func reverse_direction() -> void:
 	match $CompEnemyMove.moving_direction:
 		"right":
-			print("Move left")
 			$CompEnemyMove.move_left()
 		"left":
-			print("Move right")
 			$CompEnemyMove.move_right()
 		"up":
-			print("Move down")
 			$CompEnemyMove.move_down()
 		"down":
-			print("Move up")
 			$CompEnemyMove.move_up()
 
 func decide_left_or_right() -> String:
@@ -31,27 +28,55 @@ func decide_left_or_right() -> String:
 	print("Direction to move is ", str)
 	return str
 
+func decide_up_or_down() -> String:
+	var decision = randi_range(1, 10) % 2 == 0
+	var str : String
+	if decision == true:
+		str = "up"
+	else:
+		str = "down"
+	print("Direction to move is ", str)
+	return str
+
+func keep_on_keeping_on() -> bool:
+	return randi_range(1, 10) % 2 == 0
+
+
 func _on_right_area_body_exited(body: Node2D) -> void:
-	var decision = decide_left_or_right()
-	
-	if $CompEnemyMove.moving_direction != "left" && $CompEnemyMove.moving_direction != "right":
+	if !$LeftRaycast.is_colliding():
+		decision = decide_left_or_right()
 		if decision == "right":
 			$CompEnemyMove.move_right()
 		else:
 			$CompEnemyMove.move_left()
-	elif $CompEnemyMove.moving_direction != "left":
-		$CompEnemyMove.move_right()
+	elif $CompEnemyMove.moving_direction == "up" or $CompEnemyMove.moving_direction == "Down" && $LeftRaycast.is_colliding():
+		var yeah = keep_on_keeping_on()
+		if yeah == true:
+			$CompEnemyMove.move_right()
 
 
 func _on_left_area_body_exited(body: Node2D) -> void:
-	if $CompEnemyMove.moving_direction != "right":
-		#$CompEnemyMove.move_left()
-		print("Do we need a left ")
+	if !$RightRaycast.is_colliding():
+		pass
+	elif $RightRaycast.is_colliding():
+		$CompEnemyMove.move_left()
+
 
 func _on_upper_area_body_exited(body: Node2D) -> void:
-	if $CompEnemyMove.moving_direction != "down":
-		$CompEnemyMove.move_up()
+	if !$LowerRaycast.is_colliding():
+		decision = decide_up_or_down()
+		if decision == "up":
+			$CompEnemyMove.move_up()
+		else:
+			$CompEnemyMove.move_down()
+	elif $CompEnemyMove.moving_direction == "right" or $CompEnemyMove.moving_direction == "left" && $LowerRaycast.is_colliding():
+		var yeah = keep_on_keeping_on()
+		if yeah == true:
+			$CompEnemyMove.move_up()
+
 
 func _on_lower_area_body_exited(body: Node2D) -> void:
-	if $CompEnemyMove.moving_direction != "up":
+	if !$UpperRaycast.is_colliding():
+		pass
+	elif $UpperRaycast.is_colliding():
 		$CompEnemyMove.move_down()
